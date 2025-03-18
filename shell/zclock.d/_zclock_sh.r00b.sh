4570391879fde517aa95a143d658bea1265fea94
wa1tnr <wa1tnr@users.noreply.github.com>
2025-03-18 21:37:09 +0000

    zclock - script to print zone time samples
      print each of the 24 time zones
    
    	new file:   _zclock_sh.r00b.sh
    
    On branch dvlp-aa

---
+++ shell/zclock.d/_zclock_sh.r00b.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+# Tue 18 Mar 21:34:03 UTC 2025
+# was: Thu 13 Mar 11:08:18 UTC 2025
+
+datex() {
+    /bin/date "+%a %e %b %H:%M:%S %Z %Y"
+}
+
+legend_up() {
+    echo "Day Dt Mon hh:mm:ss UTC Year: Zone name"
+}
+
+nopayl() {
+    for zoneWanted in \
+        Atlantic/Bermuda \
+        America/Argentina/Cordoba \
+        Atlantic/South_Georgia \
+        Europe/London \
+        Asia/Tel_Aviv \
+        Asia/Yerevan \
+        Asia/Singapore \
+        Pacific/Guam \
+        Pacific/Wake
+        do
+            echo -n `TZ=$zoneWanted /bin/date`
+            echo -n ': '
+            echo $zoneWanted ' mishmash'
+        done
+}
+
+payload() {
+    for zoneWanted in \
+          \
+        Etc/GMT+12 \
+        Pacific/Midway \
+        Etc/GMT+11 \
+        Pacific/Honolulu \
+        Etc/GMT+10 \
+        US/Aleutian \
+        Etc/GMT+9 \
+        America/Anchorage \
+        Etc/GMT+8 \
+        America/Los_Angeles \
+        Etc/GMT+7 \
+        America/Denver \
+        Etc/GMT+6 \
+        America/Chicago \
+        Etc/GMT+5 \
+        America/New_York \
+        Etc/GMT+4 \
+        America/Halifax \
+        Etc/GMT+3 \
+        America/Scoresbysund \
+        Etc/GMT+2 \
+        Atlantic/Azores \
+        Etc/GMT+1 \
+        Europe/Lisbon \
+        Etc/GMT+0 \
+        Europe/Paris \
+        Etc/GMT-1 \
+        Europe/Helsinki \
+        Etc/GMT-2 \
+        Europe/Moscow \
+        Etc/GMT-3 \
+        Asia/Dubai \
+        Etc/GMT-4 \
+        Asia/Yekaterinburg \
+        Etc/GMT-5 \
+        Asia/Omsk \
+        Etc/GMT-6 \
+        Asia/Saigon \
+        Etc/GMT-7 \
+        Asia/Hong_Kong \
+        Etc/GMT-8 \
+        Asia/Tokyo \
+        Etc/GMT-9 \
+        Asia/Vladivostok \
+        Etc/GMT-10 \
+        Australia/Sydney \
+        Etc/GMT-11 \
+        Pacific/Fiji \
+        Etc/GMT-12 \
+        Pacific/Auckland \
+        Etc/GMT-13 \
+        Pacific/Kiritimati \
+        Etc/GMT-14
+        do
+            echo -n `TZ=$zoneWanted datex` ; echo -n ': '; echo $zoneWanted
+            sleep 0.3
+        done
+}
+
+aoe_payload() {
+        echo -n `TZ=Etc/GMT+12  datex` ; echo -n ': '; echo "AoE*"
+        sleep 0.3
+}
+
+# echo -n `TZ=$zoneWanted datex` ; echo -n ': '; echo $zoneWanted
+
+echo "AoE - anywhere on Earth - "
+echo "      last place to observe any given calendar date"
+echo
+
+legend_up
+aoe_payload
+# nopayl
+payload
+echo
+echo "UTC +14 first place to observe any calendar date"
+
+echo -n 'UTC: ' ; datex
+
+# end.
