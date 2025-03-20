d13950345f65dd6ebf8e1fce9feb53105da99b95
wa1tnr <wa1tnr@users.noreply.github.com>
2025-03-20 01:35:17 +0000

    add latin american cities alternative
      edit script lightly to make the choice
    
    	modified:   _zclock_sh.r00b.sh
    
    On branch dvlp-aa

--- shell/zclock.d/_zclock_sh.r00b.sh
+++ shell/zclock.d/_zclock_sh.r00b.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-# Tue 18 Mar 21:34:03 UTC 2025
+# Thu 20 Mar 01:26:11 UTC 2025
 # was: Thu 13 Mar 11:08:18 UTC 2025
+
+# Now with alternate latin_payload
+# to bias below the Equator
+# in the Americas
 
 datex() {
     /bin/date "+%a %e %b %H:%M:%S %Z %Y"
@@ -10,9 +14,13 @@ legend_up() {
     echo "Day Dt Mon hh:mm:ss UTC Year: Zone name"
 }
 
+
+# bermuda cordoba s georgia london telaviv yerevan singapor guam wake
+
 nopayl() {
     for zoneWanted in \
         Atlantic/Bermuda \
+        America/Buenos_Aires \
         America/Argentina/Cordoba \
         Atlantic/South_Georgia \
         Europe/London \
@@ -28,6 +36,43 @@ nopayl() {
         done
 }
 
+latin_payload() {
+    for zoneWanted in \
+        \
+        America/Managua \
+        Etc/GMT+6 \
+        America/Bogota \
+        Etc/GMT+5 \
+        America/Caracas \
+        Etc/GMT+4 \
+        America/Buenos_Aires \
+        Etc/GMT+3
+
+        do
+            echo -n `TZ=$zoneWanted datex` ; echo -n ': '; echo $zoneWanted
+            sleep 0.3
+	done
+}
+
+conus_payload() {
+    for zoneWanted in \
+        \
+        America/Denver \
+        Etc/GMT+6 \
+        America/Chicago \
+        Etc/GMT+5 \
+        America/New_York \
+        Etc/GMT+4 \
+        America/Halifax \
+        Etc/GMT+3
+
+        do
+            echo -n `TZ=$zoneWanted datex` ; echo -n ': '; echo $zoneWanted
+            sleep 0.3
+	done
+
+}
+
 payload() {
     for zoneWanted in \
           \
@@ -41,15 +86,17 @@ payload() {
         America/Anchorage \
         Etc/GMT+8 \
         America/Los_Angeles \
-        Etc/GMT+7 \
-        America/Denver \
-        Etc/GMT+6 \
-        America/Chicago \
-        Etc/GMT+5 \
-        America/New_York \
-        Etc/GMT+4 \
-        America/Halifax \
-        Etc/GMT+3 \
+        Etc/GMT+7
+        do
+            echo -n `TZ=$zoneWanted datex` ; echo -n ': '; echo $zoneWanted
+            sleep 0.3
+        done
+
+    conus_payload
+    # latin_payload
+
+    for zoneWanted in \
+          \
         America/Scoresbysund \
         Etc/GMT+2 \
         Atlantic/Azores \
@@ -107,7 +154,12 @@ aoe_payload
 payload
 echo
 echo "UTC +14 first place to observe any calendar date"
-
+echo
+echo '    ' conus_payload: Denver, Chicago, New_York and Halifax
+echo '    ' latin_payload: Managua, Bogota, Caracas and Buenos_Aires
+echo
+echo "vsc-wokwi-328p-aa/oob.d/zclock.d/_zclock_sh.r00a.sh"
+echo
 echo -n 'UTC: ' ; datex
 
 # end.
